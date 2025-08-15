ccc9b0957573f6c80f7030c3a6026c517acf90d2
Diego Leão <diegoleaovelame@hotmail.comgit commit -m closes>
2025-08-15 18:58:22 -0300

    closes #5 - Concluído: ajuste do mecanismo de quantidade de itens com javascript.

--- assets/js/function-calc.js
+++ assets/js/function-calc.js
@@ -1,6 +1,10 @@
 var prods = [
-    {id: 1, name: "Produto 1", price: 10.0 },
-    {id: 2, name: "Produto 2", price: 20.0},
+    {id: 1, name: "Bife com batata", price: 30.0 }, 
+    {id: 2, name: "Coxa de frango crocante", price: 25.0 },
+    {id: 3, name: "Carne de panela", price: 22.0 },
+    {id: 4, name: "Farofa", price: 10.0 },
+    {id: 5, name: "Salada", price: 8.0 },
+    {id: 6, name: "Torresmo", price: 12.0 }
 ];
 
 function calc(){
@@ -8,14 +12,18 @@ function calc(){
     var output = document.getElementById("output");
     var total = 0;
 
-    output.innerHTML = "";
+
+    output.innerHTML = "<h2>Resumo do Pedido:</h2>";
 
     for (var input of quantities) {
         var id = input.id;
+        var qtd = parseInt(input.value);
 
-        output.innerHTML += `Produto ${prods[id-1].name} - Preço ${prods[id-1].price} - Quantidade ${input.value} </br>`;
-        total += prods[id-1].price * parseFloat(input.value); //ATENÇÃO: Parse float não é necessário neste caso, apenas é necessário em situações de soma em que javascript pode confundir a operação de soma com concatenação.
+        if(qtd > 0) {
+            output.innerHTML += `<div class= checkout_itens><p>Produto: ${prods[id-1].name}</p> </br> <p>Quantidade: ${input.value}</p></br> <p>Preço: ${prods[id-1].price.toFixed(2)}</p></br></div>`;
+            total += prods[id-1].price * parseFloat(input.value); //ATENÇÃO: Parse float não é necessário neste caso, apenas é necessário em situações de soma em que javascript pode confundir a operação de soma com concatenação.    
+        }
     }
 
-    output.innerHTML += `<h2>Total: ${total}</h2>`;
+    output.innerHTML += `<h2>Total: ${total.toFixed(2)}</h2>`;
 }
